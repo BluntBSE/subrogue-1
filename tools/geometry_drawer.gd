a25b3b52121aa877e7b0742521d99a4939da0b28
Robert Meyer <ram.news1@gmail.com>
2025-02-06 16:38:19 -0500

    can project custom mesh into sphere with drawing tool

--- tools/geometry_drawer.gd
+++ tools/geometry_drawer.gd
@@ -14,7 +14,8 @@ var indices := PackedInt32Array()
 var vertices := PackedVector3Array()
 var normals := PackedVector3Array()
 @export var bake_mesh:bool = false
-
+var extrude_up = 3.0
+var extrude_down = 7.0
 
 signal add_vertex_at
 
@@ -37,7 +38,7 @@ func _process(delta: float) -> void:
         dh.dprint(debug, dstr)
         
     if bake_mesh == true:
-        generate_mesh_from_vertices(vertices, indices, normals)
+        generate_mesh_from_vertices(vertices)
         bake_mesh = false
         
     input_clicks()
@@ -72,7 +73,6 @@ func cast_from_camera()->Dictionary:
 func input_clicks():
     #BECAUSE we're not shift clicking or doing waypoint logic right now, this always overwrites the most recent item in the queue.
     if Input.is_action_just_pressed("editor_apply_to"):
-        print("Foo!")
         if hovering_over != {}:
             if hovering_over.collider.name == draw_on.name:
                 print("Clicked on collider", hovering_over)
@@ -98,8 +98,11 @@ func input_clicks():
 #REWIND POINT
 func handle_add_vertex_at(position: Vector3) -> void:
     print("Hello from handle_add_vertex_at")
-    vertices.append(position)
-    normals.append(normal_from_vertex(position, draw_on))
+    var direction_from_anchor = normal_from_vertex(position, draw_on)
+    var new_position = position + (direction_from_anchor * extrude_up)
+    
+    
+    vertices.append(new_position)
     
     #DEBUG SPHERES
     var mesh := SphereMesh.new()
@@ -109,50 +112,54 @@ func handle_add_vertex_at(position: Vector3) -> void:
     mesh.radius = radius
     mesh.height = height
     sphere.mesh = mesh
-    sphere.position = position # Equivalent to transform.origin
+    var sphere_2 = sphere.duplicate()
+   # var sphere_3 = sphere.duplicate()
+    sphere.position = new_position # Equivalent to transform.origin
+    sphere_2.position = position
+    #sphere_3.position = 
     add_child(sphere)
+    add_child(sphere_2)
     debug_spheres.append(sphere)
+    debug_spheres.append(sphere_2)
     
-    if vertices.size() >= 4:
-        generate_mesh_from_vertices(vertices, indices, normals)
     
 
 
-func generate_mesh_from_vertices(_vertices: PackedVector3Array, _indices: PackedInt32Array, _normals: PackedVector3Array):
-    arrays.resize(Mesh.ARRAY_MAX)
-    arrays[Mesh.ARRAY_VERTEX] = _vertices
-    arrays[Mesh.ARRAY_INDEX] = _indices
-    arrays[Mesh.ARRAY_NORMAL] = _normals
+func generate_mesh_from_vertices(_vertices: PackedVector3Array):
     var surface_tool := SurfaceTool.new()
     surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
     
-    #This function is called only when there are 4 vertices available, so:
-    var v0 = _vertices[0]
-    var v1 = _vertices[1]
-    var v2 = _vertices[2]
-    var v3 = _vertices[3]
+    # Every vertex in _vertices represents the high point of the plane we want to generate
+    var vertices_upper = _vertices.duplicate()
+    vertices_upper.reverse() # Start at the last point drawn, not the first.
+    var vertices_lower = PackedVector3Array()
     
-    surface_tool.set_normal(normal_from_vertex(v0, draw_on))
-    surface_tool.add_vertex(v0) # Top left
-    surface_tool.set_normal(normal_from_vertex(v1, draw_on))
-    surface_tool.add_vertex(v1) # Top right
-    surface_tool.set_normal(normal_from_vertex(v2, draw_on))
-    surface_tool.add_vertex(v2) # Bottom left
-
-    surface_tool.set_normal(normal_from_vertex(v2, draw_on))
-    surface_tool.add_vertex(v2) # Bottom left
-    surface_tool.set_normal(normal_from_vertex(v1, draw_on))
-    surface_tool.add_vertex(v1) # Top right
-    surface_tool.set_normal(normal_from_vertex(v3, draw_on))
-    surface_tool.add_vertex(v3) # Bottom right
+    # Create a mirror vertex beneath the anchor's surface
+    for vertex in vertices_upper:
+        var l_dir = (vertex - draw_on.position).normalized() * -1.0
+        var l_dist = l_dir * extrude_down
+        var lower_vertex: Vector3 = vertex + l_dist
+        vertices_lower.append(lower_vertex)
     
-   # surface_tool.generate_normals()
+    # 0,1,2 | 2, 1, 3 Counter-clockwise winding order.
+    
+    for i in range(vertices_upper.size() - 1):
+        # 0, 1, 2
+        print(i)
+        surface_tool.add_vertex(vertices_upper[i])
+        surface_tool.add_vertex(vertices_lower[i])
+        surface_tool.add_vertex(vertices_upper[i + 1])
+        
+        surface_tool.add_vertex(vertices_lower[i])
+        surface_tool.add_vertex(vertices_lower[i + 1])
+        surface_tool.add_vertex(vertices_upper[i + 1])
+    
+    # surface_tool.generate_normals()
     var mesh = surface_tool.commit()
     var mesh_instance = MeshInstance3D.new()
     mesh_instance.mesh = mesh
     add_child(mesh_instance)
     mesh_instance.owner = get_tree().edited_scene_root
-    
     
     print("MeshInstance3D created and added to the scene.")
     
