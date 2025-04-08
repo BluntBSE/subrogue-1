acb57f5da66cd4882a942165a9da031320287ceb
bluntbse <ram.news1@gmail.com>
2025-04-08 17:23:03 -0400

    oops

--- engine/networking/steam/steam_manager.gd
+++ engine/networking/steam/steam_manager.gd
@@ -23,123 +23,123 @@ signal lobby_ready #I'm positive there's a built in way to do this, but I couldn
 #Actually, it appears that callbacks only work if the peer etc. is in the node, so duplicating these is necessary if I want this global
 #The below signals actually work if I wire up UI to the Steam callbacks directly
 #But I actually want the global modification of the client variables in one place
-#So I duplicate the signals here. 
+#So I duplicate the signals here.
 signal self_joined_new_lobby
 signal lobby_joined
 func _init()->void:
-    print("Initializing Steam")
-    #What's the difference between AppID and GameID?
-    OS.set_environment("SteamAppID", str(steam_app_id))
-    OS.set_environment("SteamGameID", str(steam_app_id))
-    Steam.connect("lobby_joined", on_lobby_joined)
+	print("Initializing Steam")
+	#What's the difference between AppID and GameID?
+	OS.set_environment("SteamAppID", str(steam_app_id))
+	OS.set_environment("SteamGameID", str(steam_app_id))
+	Steam.connect("lobby_joined", on_lobby_joined)
 
 func _process(delta:float)->void:
-    Steam.run_callbacks()
+	Steam.run_callbacks()
 
 func initialize_steam():
-    var initialize_response:Dictionary = Steam.steamInitEx()
-    print ("Did steam initialize? %s" % initialize_response)
-    if initialize_response['status'] > 0:
-        print("Steam did not initialize! Perish, fool")
-        get_tree().quit()
-    
-    is_owned = Steam.isSubscribed()
-    steam_id = Steam.getSteamID()
-    online = Steam.loggedOn()
-    steam_username = Steam.getPersonaName()
-    
-    
-func _ready():
-    initialize_steam()
-    #TODO: Probably need to lift this multiplayer peer out if we do direct networking
-    print("MY PEER IS: ")
-    print(multiplayer.multiplayer_peer)
-    multiplayer_peer.lobby_created.connect(on_lobby_created)
-    multiplayer_peer.lobby_joined.connect(on_lobby_joined)
+	var initialize_response:Dictionary = Steam.steamInitEx()
+	print ("Did steam initialize? %s" % initialize_response)
+	if initialize_response['status'] > 0:
+		print("Steam did not initialize! Perish, fool")
+		get_tree().quit()
 
-    #THere's some kinda lobby chat methods built in on the peer?
+	is_owned = Steam.isSubscribed()
+	steam_id = Steam.getSteamID()
+	online = Steam.loggedOn()
+	steam_username = Steam.getPersonaName()
+
+
+func _ready():
+	initialize_steam()
+	#TODO: Probably need to lift this multiplayer peer out if we do direct networking
+	print("MY PEER IS: ")
+	print(multiplayer.multiplayer_peer)
+	multiplayer_peer.lobby_created.connect(on_lobby_created)
+	multiplayer_peer.lobby_joined.connect(on_lobby_joined)
+
+	#THere's some kinda lobby chat methods built in on the peer?
 
 
 func become_host():
-    if SteamManager.hosted_lobby_id != 0:
-        print("Lobby already running! It's: ", SteamManager.hosted_lobby_id)
-        return
-    print("Starting host!")
-    #TODO: Make lobby type configurable. Probably has an argument to this function
-    multiplayer_peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC, 6)
-    multiplayer.multiplayer_peer = multiplayer_peer
-    print("HOSTED! Godot MP peer is ", multiplayer.multiplayer_peer)
-    #hosted_lobby_id is set by the callback triggered by the above
-    if not OS.has_feature("dedicated_server"):  # Not doing this yet.
-        pass
-        
+	if SteamManager.hosted_lobby_id != 0:
+		print("Lobby already running! It's: ", SteamManager.hosted_lobby_id)
+		return
+	print("Starting host!")
+	#TODO: Make lobby type configurable. Probably has an argument to this function
+	multiplayer_peer.create_lobby(SteamMultiplayerPeer.LOBBY_TYPE_PUBLIC, 6)
+	multiplayer.multiplayer_peer = multiplayer_peer
+	print("HOSTED! Godot MP peer is ", multiplayer.multiplayer_peer)
+	#hosted_lobby_id is set by the callback triggered by the above
+	if not OS.has_feature("dedicated_server"):  # Not doing this yet.
+		pass
+
 func join_as_client(_lobby_id):
-    #Clear previous lobby members
-    lobby_members.clear()
-    print("Joining lobby as client, lobby id: ", str(_lobby_id))
-    multiplayer_peer.connect_lobby(_lobby_id)
-    lobby_id = _lobby_id
-    self_joined_new_lobby.emit(_lobby_id)
-    #Godot peer = steam peer. 
-    multiplayer.multiplayer_peer = multiplayer_peer
-    print("Joined! Godot MP peer is", multiplayer.multiplayer_peer)
-    #TODO: Where to add players to the lobby members array?
-    #Tempted to do it here but the tutorial did not...
+	#Clear previous lobby members
+	lobby_members.clear()
+	print("Joining lobby as client, lobby id: ", str(_lobby_id))
+	multiplayer_peer.connect_lobby(_lobby_id)
+	lobby_id = _lobby_id
+	self_joined_new_lobby.emit(_lobby_id)
+	#Godot peer = steam peer.
+	multiplayer.multiplayer_peer = multiplayer_peer
+	print("Joined! Godot MP peer is", multiplayer.multiplayer_peer)
+	#TODO: Where to add players to the lobby members array?
+	#Tempted to do it here but the tutorial did not...
 
 func _add_player_to_lobby():
-    pass
+	pass
 
 func _disconnect_player():
-    # Remove from lobby if a game hasn't started
-    # Remove from game if they have
-    pass
+	# Remove from lobby if a game hasn't started
+	# Remove from game if they have
+	pass
 
 func on_lobby_created(connect: int, _lobby_id):
-    print("On lobby created")
-    print("Peer ID", connect)
-    print("Lobby ID", lobby_id)
-    if connect == 1:
-        hosted_lobby_id = _lobby_id
-        lobby_id = _lobby_id
-        print("Created lobby, lobby id: ", str(hosted_lobby_id))
-        
-        Steam.setLobbyJoinable(hosted_lobby_id, true)
-        Steam.setLobbyData(hosted_lobby_id, "name", LOBBY_NAME)
-        Steam.setLobbyData(hosted_lobby_id, "mode", LOBBY_MODE)
-        print("Lobby ready should emit")
-        lobby_ready.emit(connect, lobby_id)
+	print("On lobby created")
+	print("Peer ID", connect)
+	print("Lobby ID", lobby_id)
+	if connect == 1:
+		hosted_lobby_id = _lobby_id
+		lobby_id = _lobby_id
+		print("Created lobby, lobby id: ", str(hosted_lobby_id))
+
+		Steam.setLobbyJoinable(hosted_lobby_id, true)
+		Steam.setLobbyData(hosted_lobby_id, "name", LOBBY_NAME)
+		Steam.setLobbyData(hosted_lobby_id, "mode", LOBBY_MODE)
+		print("Lobby ready should emit")
+		lobby_ready.emit(connect, lobby_id)
 
 
 func list_lobbies():
-    print("Listing steam lobbies")
-    Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE) #LOBBY_DISTANCE_FILTER_DEFAULT
-    Steam.requestLobbyList()
-    pass
+	print("Listing steam lobbies")
+	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE) #LOBBY_DISTANCE_FILTER_DEFAULT
+	Steam.requestLobbyList()
+	pass
 
 
 func on_lobby_joined(lobby: int, permissions: int, locked: bool, response: int):
-    print("SteamManager: on lobby joined")
+	print("SteamManager: on lobby joined")
 
-    #LOBBY_NAME = Steam.getLobbyData(lobbyID, "name")
-    get_lobby_members()
-    lobby_joined.emit(lobby, permissions, locked, response)
+	#LOBBY_NAME = Steam.getLobbyData(lobbyID, "name")
+	get_lobby_members()
+	lobby_joined.emit(lobby, permissions, locked, response)
 
-    
+
 func get_lobby_members():
-    lobby_members.clear()
-    var num_members = Steam.getNumLobbyMembers(lobby_id)
-    
-    for member in range(num_members):
-        var member_id = Steam.getLobbyMemberByIndex(lobby_id,member)
-        var member_name = Steam.getFriendPersonaName(member_id)
-        lobby_members.append({"steam_id":member_id, "steam_name":member_name})
+	lobby_members.clear()
+	var num_members = Steam.getNumLobbyMembers(lobby_id)
+
+	for member in range(num_members):
+		var member_id = Steam.getLobbyMemberByIndex(lobby_id,member)
+		var member_name = Steam.getFriendPersonaName(member_id)
+		lobby_members.append({"steam_id":member_id, "steam_name":member_name})
 
 func add_player_list():
-    pass
-    
+	pass
+
 func on_lobby_join_requested(_lobbyID, friendID):
-    var owner_name = Steam.getFriendPersonaName(friendID)
-    
-    #Message here if you want
-    
-    join_as_client(_lobbyID)
+	var owner_name = Steam.getFriendPersonaName(friendID)
+
+	#Message here if you want
+
+	join_as_client(_lobbyID)
--- engine/root/mp_globe_spawner.gd
+++ engine/root/mp_globe_spawner.gd
@@ -2,16 +2,20 @@ extends MultiplayerSpawner
 class_name GlobeSpawner
 
 func _ready():
-    spawn_function = spawn_globe
+	pass
+	#Maybe noneedfor custom spawner yet
+	#spawn_function = spawn_globe
 
 func spawn_globe():
-    #List all peers this will effect:
-    var peers = multiplayer.get_peers()
-    print("PEERS FOR SPAWN ARE: ", peers)
-    
-    #WARNING: Preloading here moves the load time to when the application first boots. That may or may not be okay.
-    #In fact it seems preferable right now, but if it becomes an issue this is to blame.
-    var globe_root:GlobeRoot = preload("res://globe_scene/globe_scene.tscn").instantiate()
-    var mainmenu = get_tree().root.find_child("MainMenu", true, false)
-    mainmenu.queue_free()
-    return globe_root
+	#List all peers this will effect:
+	var peers = multiplayer.get_peers()
+	print("PEERS FOR SPAWN ARE: ", peers)
+
+	#WARNING: Preloading here moves the load time to when the application first boots. That may or may not be okay.
+	#In fact it seems preferable right now, but if it becomes an issue this is to blame.
+	var globe_root:GlobeRoot = preload("res://globe_scene/globe_scene.tscn").instantiate()
+	var mainmenu = get_tree().root.find_child("MainMenu", true, false)
+	mainmenu.queue_free()
+	var game_root = get_tree().root.find_child("GameRoot", true, false)
+	game_root.add_child(globe_root)
+	return globe_root
