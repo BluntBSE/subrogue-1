0938fce8041e8916b45b6baed9b9762cc7bf9367
Robert Meyer <ram.news1@gmail.com>
2025-04-27 14:18:53 -0400

    pre-heap experimentation

---
+++ engine/cities/city_defs/city_def.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name CityDef
+
+@export var city_name:String
+#Faction ownership is determined by the tree, not the.
+@export_multiline var city_greeting:String
+@export var city_portrait:Texture2D
