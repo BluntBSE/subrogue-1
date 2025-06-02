5719b95ca2f67088052f4457a4e8b206948c550c
Rowan <ram.news1@gmail.com>
2025-06-02 01:09:48 -0400

    splitting up quest render

---
+++ engine/cities/city_ui/contract_close_up.gd
@@ -0,0 +1,7 @@
+extends Control
+class_name ContractCloseUp
+
+
+@onready var contract_npc_label = %ContractNPCLabel
+@onready var mission_details_label = %MissionDetailsLabel
+@onready var mission_details_sub_label = %MissionDetailsLabel
